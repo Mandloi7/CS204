e860058362211fcc7fd68740318beb2592ae7e58
Ritik Mandloi <45793266+Mandloi7@users.noreply.github.com>
2019-08-19 22:54:54 +0530

    Update EXP_TREE.cpp

--- LAB_4/EXP_TREE.cpp
+++ LAB_4/EXP_TREE.cpp
@@ -2,11 +2,11 @@
 using namespace std;
 
 struct tr{
-    char value;
+    string value;
     tr *left,*right ;
 };
 
- struct tr* newNode(char a){
+ struct tr* newNode(string a){
     tr* tp=new tr;
     tp->left=NULL;
     tp->right=NULL;
@@ -17,7 +17,7 @@ struct tr{
 bool isOperator(char c) 
 { 
     if (c == '+' || c == '-' || 
-            c == '*' || c == '/' || 
+            c == '*'|| c =='/' || 
             c == '^') 
         return true; 
     return false; 
@@ -34,95 +34,120 @@ int prec(char c)
     return -1; 
 } 
 
-string ToPostfix(string s) 
-{ 
-    stack<char> st; 
-    st.push('N'); 
-    int l = s.length(); 
-    string ns; 
-    for(int i = 0; i < l; i++) 
-    { 
-        // If the scanned character is an operand, add it to output string. 
-        if(isdigit(s[i]) )
-        ns+=s[i]; 
-  
-        // If the scanned character is an ‘(‘, push it to the stack. 
-        else if(s[i] == '(') 
-          
-        st.push('('); 
-          
-        // If the scanned character is an ‘)’, pop and to output string from the stack 
-        // until an ‘(‘ is encountered. 
-        else if(s[i] == ')') 
-        { 
-            while(st.top() != 'N' && st.top() != '(') 
-            { 
-                char c = st.top(); 
-                st.pop(); 
-               ns += c; 
-            } 
-            if(st.top() == '(') 
-            { 
-                char c = st.top(); 
-                st.pop(); 
-            } 
-        } 
-          
-        //If an operator is scanned 
-        else{ 
-            while(st.top() != 'N' && prec(s[i]) >= prec(st.top())) 
-            { 
-                char c = st.top(); 
-                st.pop(); 
-                ns += c; 
-            } 
-            st.push(s[i]); 
-        } 
-  
-    } 
-    //Pop all the remaining elements from the stack 
-    while(st.top() != 'N') 
-    { 
-        char c = st.top(); 
-        st.pop(); 
-        ns += c; 
-    } 
-      
- return ns;
-  
-} 
+string inpost (string s)
+{
+	stack<char> a;
+	string post = "";
 
+	for(int i=0; i<s.length(); i++)
+	{
+	    int flag=0;
+		if(s[i] >= '0' && s[i] <= '9')
+			{
+				while(s[i] >= '0' && s[i] <= '9')
+				{
+					post += s[i];
+				i++;
+				}
+
+				post += " ";
+				i--;
+			}
+
+		else if(s[i] == '(')
+			a.push(s[i]);
+
+		else if(s[i] == ')')
+		{
+			while(a.empty() == 0 && a.top() != '(')
+			{
+				post += a.top();
+				a.pop();
+			}
+
+			if(a.top() == '(')
+			{
+			
+				a.pop();
+			}
+		}
+
+		else
+		{
+		    if(prec(s[i]==3)&&(a.empty()==0)&&(prec(a.top())==3)){
+                    a.push(s[i]);
+                    
+                    continue;
+                }
+			while(a.empty() == 0 && prec(s[i]) <= prec(a.top())) 
+            { 
+                
+                char c = a.top(); 
+                a.pop(); 
+                post += c;
+                post += " ";
+            } 
+            a.push(s[i]); 
+		}
+		
+	}
+
+		while(a.empty() == 0) 
+    { 
+        char c = a.top(); 
+        a.pop(); 
+        post += c; 
+        post += " ";
+    } 
+	
+	return post;
+}
 
 tr* CreateTree(string postfix) 
 { 
     stack<tr *> st; 
     tr *t, *t1, *t2; 
   
-    // Traverse through every character of 
-    // input expression 
+    
     for (int i=0; i<postfix.length(); i++) 
     { 
-        // If operand, simply push into stack 
-        if (!isOperator(postfix[i])) 
+        if(postfix[i]==' '){continue;}
+        
+        if (!isOperator(postfix[i]) )
         { 
-            t = newNode(postfix[i]); 
-            st.push(t); 
+             
+            int z=0;
+            for(int j=i;j<postfix.length();j++){
+                if(postfix[j]>=48 && postfix[j]<=57){
+                    i=j;
+                    z=10*z+postfix[j]-48;
+                }
+                else{
+                    break;
+                }
+            }
+            //cout << z << "\n";
+            string g=to_string(z);
+            t=newNode(g);
+            st.push(t);
+            continue;
         } 
-        else // operator 
+        else
         { 
-            t = newNode(postfix[i]); 
+            string st2(1,postfix[i]);
+            t = newNode(st2); 
   
-            // Pop two top nodes 
-            t1 = st.top(); // Store top 
-            st.pop();      // Remove top 
+             
+            t1 = st.top();  
+            st.pop();      
             t2 = st.top(); 
             st.pop(); 
   
-            //  make them children 
+             
             t->right = t1; 
             t->left = t2; 
   
-            // Add this subexpression to stack 
+             
             st.push(t); 
         } 
     } 
@@ -134,53 +159,64 @@ tr* CreateTree(string postfix)
   
     return t; 
 } 
-int evaluate(tr* node){
-    if(!isOperator){
-        return node->value;
-    }else{
-        int val;
-        int l=evaluate(node->left);
-        int r=evaluate(node->right);
-        switch (node->value){
-            case '+' : 
-            val=l+r ;
-            break;
-            case '-' : 
-            val=l-r ;
-            break;
-            case '*' : 
-            val=l*r ;
-            break;
-            case '/' : 
-            val=l/r ;
-            break;
-            case '^' : 
-            val=l^r ;
-            break;
-            
-        }
-    }
-} 
+
+
+int evaluate (struct tr *root)
+{
+    if(root->left == NULL && root->right == NULL)
+		return stoi(root->value);
+/*else if(root == NULL)
+	{
+		cout << "EMPTY, CANNOT BE EVALUATED\n";
+		return 0;
+	}*/
+
+	
+	else
+	{
+		int r = evaluate(root->right);
+		int l = evaluate(root->left);
+
+		    if(root->value=="+"){return l+r;}
+		  if(root->value=="-"){return l-r;}
+		  if(root->value=="*"){return l*r;}
+		  if(root->value=="/"){return l/r;}
+		  if(root->value=="^"){return (int)pow(l,r);}
+		
+	}
+}
+
 int main() {
-    int t;
+  int t;
     cin >> t;
-    while(t--){
+    while(t){
     
 int n;
 cin >> n;
-string a[n];
-string b[n];
+while(n){
 int i;
 for(i=0;i<n;i++){
-cin >> a[i];
-b[i]=ToPostfix(a[i]);
+string a;
+string b;
+cin >> a;
+b=inpost(a);
 
-tr* root=CreateTree(b[i]);
+
+
+
+//cout << b<<"\n";
+//CreateTree(b);
+struct tr* root=CreateTree(b);
+//cout << root->value ;
 int ans=evaluate(root);
 cout << ans << endl;
-
-
+n--;
 }
 }
-	return 0;
+
+        t--;
+    }
+
+
+return 0;
 }
