68238cd17545c825ce7d76d2eb242fce36276619
Ritik Mandloi <45793266+Mandloi7@users.noreply.github.com>
2019-11-15 07:14:09 +0530

    Update Code1.cpp

--- POST_MIDSEM_LAB_6/Code1.cpp
+++ POST_MIDSEM_LAB_6/Code1.cpp
@@ -10,7 +10,6 @@ int main()
 	lli n;
 	cin >> n;
 	vector<string> a, b;
-	vector<string> p;
 	for(lli i=0; i<n; i++)
 	{   
 	    string s;
@@ -21,12 +20,9 @@ int main()
 		{
 			a.push_back(s);
 			b.push_back(r);
-		}else{
-		    p.push_back(s);
 		}
 	}
 	sort(a.begin(), a.end());
-	sort(p.begin(), p.end());
 	for(lli i=0; i<b.size(); i++)
 	{
 		if (binary_search(a.begin(), a.end(), b[i]))
@@ -36,13 +32,7 @@ int main()
 		}
 		
 	}
-	for(lli i=0; i<p.size()-1; i++)
-	{
-		if(p[i]==p[i+1]){
-		    cout << "YES\n";
-		    return 0;
-		}
-	}
+	
 	cout << "NO\n";
 	return 0;
 }
